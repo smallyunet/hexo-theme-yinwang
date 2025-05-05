9a8ca1fd419304a584e25022d995c278e3187dd0
smallyunet <dev.wangyu@gmail.com>
2025-05-05 19:26:56 +0800

    fix: correct blockquote, list styles, and remote preload of micro blog data in JS
    Signed-off-by: smallyunet <dev.wangyu@gmail.com>

--- source/js/main.js
+++ source/js/main.js
@@ -3,7 +3,7 @@ function getUrlRelativePath() {
   var arrUrl = url.split("//");
 
   var start = arrUrl[1].indexOf("/");
-  var relUrl = arrUrl[1].substring(start); 
+  var relUrl = arrUrl[1].substring(start);
 
   if (relUrl.indexOf("?") != -1) {
     relUrl = relUrl.split("?")[0];
@@ -11,33 +11,8 @@ function getUrlRelativePath() {
   return relUrl;
 }
 
-function preloadMicroBlog(year, forceReload = false) {
-  let cacheKey = `micro-blog-${year}`;
-  let cacheDateKey = `${cacheKey}-date`;
-  let cached = localStorage.getItem(cacheKey);
-  let cacheDate = localStorage.getItem(cacheDateKey);
-
-  if (!cached || forceReload) {
-    let url = `/micro-blog/${year}.json`;
-    $.get(url, (res) => {
-      localStorage.setItem(cacheKey, JSON.stringify(res));
-      localStorage.setItem(cacheDateKey, new Date().toISOString());
-      console.log(`Data for year ${year} updated and cached`);
-    }).fail(() => {
-      console.error(`Failed to preload data for year ${year}`);
-    });
-  } else {
-    console.log(`Using cached data for year ${year}`);
-  }
-}
-
 $(() => {
   document.querySelectorAll("pre code").forEach((block) => {
     hljs.highlightBlock(block);
   });
-
-  const currentYear = new Date().getFullYear();
-  for (let year = currentYear; year >= 2020; year--) {
-    preloadMicroBlog(year, year === currentYear);
-  }
 });
